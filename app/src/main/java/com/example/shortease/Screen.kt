cccc7981b04a152e8689df0a2bdabe5733a57c3a
Anthony Truong <anthonywtruong@gmail.com>
2023-06-13 21:12:15 -0400

    add and edit files for video editor

--- app/src/main/java/com/example/shortease/Screen.kt
+++ app/src/main/java/com/example/shortease/Screen.kt
@@ -5,4 +5,5 @@ sealed class Screen(val route: String) {
     object HomeScreen: Screen(route = "home_screen")
     object MyVideos: Screen(route = "my_videos")
     object PreviewScreen: Screen(route = "preview_screen")
+    object VideoEditorScreen: Screen(route = "video_editor_screen")
 }
