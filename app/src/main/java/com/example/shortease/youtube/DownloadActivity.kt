903b3b883852addb56e8bb94ecc6bd145e17ff3f
Hanu <hanumayil@gmail.com>
2023-07-05 13:12:23 -0400

    got video to fully download (testing speeds)

--- app/src/main/java/com/example/shortease/youtube/DownloadActivity.kt
+++ app/src/main/java/com/example/shortease/youtube/DownloadActivity.kt
@@ -21,6 +21,9 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -34,60 +37,18 @@ import com.github.kiulian.downloader.downloader.request.RequestVideoFileDownload
 import com.github.kiulian.downloader.downloader.request.RequestVideoInfo
 import com.github.kiulian.downloader.downloader.response.Response
 import com.github.kiulian.downloader.model.videos.VideoInfo
+import com.github.kiulian.downloader.model.videos.formats.VideoWithAudioFormat
 import java.io.File
 
 
+
 class DownloadActivity : ComponentActivity() {
+    var downloader_progress = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // init downloader with default config
-        val downloader = YoutubeDownloader()
-        val videoId = "icPHcK_cCF4" // for url https://www.youtube.com/watch?v=abc12345
-
-        val request = RequestVideoInfo(videoId)
-            .callback(object : YoutubeCallback<VideoInfo?> {
-                override fun onFinished(videoInfo: VideoInfo?) {
-                    Log.d("youtube init","Finished parsing")
-                }
-
-                override fun onError(throwable: Throwable) {
-                    Log.d("youtube init","Error: " + throwable.message)
-                }
-            })
-            .async()
-        val response = downloader.getVideoInfo(request)
-        val video = response.data() // will block thread
-
-        Log.d("youtube init", video.toString())
-
-        val outputDir = filesDir
-        val format = video.bestVideoWithAudioFormat();
-
-// Async downloading with callback
-        val download_request = RequestVideoFileDownload(format)
-            .callback(object : YoutubeProgressCallback<File> {
-                override fun onDownloading(progress: Int) {
-                    println("Downloaded $progress%")
-                }
-
-                override fun onFinished(videoInfo: File) {
-                    Log.d("youtube init", "FINISHED DONE")
-
-                }
-
-                override fun onError(throwable: Throwable) {
-                    println("Error: ${throwable.localizedMessage}")
-                }
-            })
-            .saveTo(outputDir) // Set the output directory to your app's local storage
-            .async()
-
-        val download_response: Response<File> = downloader.downloadVideoFile(download_request)
-        val data: File = download_response.data() // This will block the current thread
-        Log.d("youtube init", data.toString())
-
         setContent {
+
             Box(
                 modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center
@@ -111,7 +72,7 @@ class DownloadActivity : ComponentActivity() {
 
                             )
                             Text(
-                                text = "Downloading Video",
+                                text = "Downloading Video ${downloader_progress}",
                                 color = colorPalette.ShortEaseRed,
                                 fontSize = 22.sp,
                                 modifier = Modifier
@@ -122,6 +83,76 @@ class DownloadActivity : ComponentActivity() {
                 }
             }
         }
+        // init downloader with default config
+        val downloader = YoutubeDownloader()
+        downloader.config.maxRetries = 1000000
+        val videoId = "K8TPaOnpl8U" // for url https://www.youtube.com/watch?v=abc12345
+
+        val request = RequestVideoInfo(videoId)
+            .callback(object : YoutubeCallback<VideoInfo?> {
+                override fun onFinished(videoInfo: VideoInfo?) {
+                    Log.d("youtube init","Finished parsing")
+                }
+
+                override fun onError(throwable: Throwable) {
+                    Log.d("youtube init","Error: " + throwable.message)
+                }
+            })
+            .async()
+        val response = downloader.getVideoInfo(request)
+        val video = response.data() // will block thread
+
+        Log.d("youtube init", video.toString())
+
+        val outputDir = filesDir
+
+
+//        var selectedFormat: VideoWithAudioFormat? = null
+//
+//        for (format in video.videoWithAudioFormats()) {
+//            if (format.videoQuality().toString().contains("medium")) {
+//                selectedFormat = format
+//                Log.d("youtube init", "${format.videoQuality()}")
+//                break
+//            }
+//        }
+//
+// //If no 720p format was found, select the best available format
+//        if (selectedFormat == null) {
+//            selectedFormat = video.videoWithAudioFormats().get(0)
+//        }
+
+
+        val format = video.bestVideoWithAudioFormat()
+
+        // Async downloading with callback
+        val download_request = RequestVideoFileDownload(format)
+            .callback(object : YoutubeProgressCallback<File> {
+                override fun onDownloading(progress: Int) {
+                    Log.d("youtube init", "Downloaded $progress%")
+                    downloader_progress = progress
+                }
+
+                override fun onFinished(videoInfo: File) {
+                    Log.d("youtube init", "FINISHED DONE")
+
+                }
+
+                override fun onError(throwable: Throwable) {
+                    Log.d("youtube init", "Error: ${throwable.localizedMessage}")
+
+                }
+            })
+            .maxRetries(100000000)
+            .saveTo(outputDir) // Set the output directory to your app's local storage
+            .async()
+
+        val download_response: Response<File> = downloader.downloadVideoFile(download_request)
+        //val data: File = download_response.data() // This will block the current thread
+        //Log.d("youtube init", data.toString())
+
+
+
     }
     fun downloadYouTubeVideo(context: Context, videoUrl: String, videoTitle: String) {
         val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
