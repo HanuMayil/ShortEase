fc729773fd9f5c22a4237a2788549c146524e6a2
Akshayan Ahilan <akshayan9221@gmail.com>
2023-07-23 14:18:36 -0400

    added videoplayer for preview screen

--- app/src/main/java/com/example/shortease/PreviewScreen.kt
+++ app/src/main/java/com/example/shortease/PreviewScreen.kt
@@ -1,3 +1,5 @@
+import android.graphics.ColorMatrix
+import android.util.Log
 import android.widget.VideoView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -12,8 +14,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,8 +40,16 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.shortease.R
 import com.example.shortease.Screen
+import com.example.shortease.endCropTime
+import com.example.shortease.playerView
+import com.example.shortease.startCropTime
 import com.example.shortease.ui.theme.ShortEaseTheme
 import com.example.shortease.ui.theme.colorPalette
+import com.example.shortease.videoDuration
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -49,7 +63,15 @@ fun PreviewScreen(
     navController: NavController
 ) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val videoId = navBackStackEntry?.arguments?.getString("videoId")
+    val context = LocalContext.current
+    val videoId = "zU9y354XAgM"
+    val videoPath = "${context.filesDir}/videos/${videoId}"
+    val folder = File(videoPath)
+    val folderContents = folder.listFiles()
+    val folderContentNames = folderContents?.map { file -> file.name } ?: emptyList()
+    var finalVideoPath = "${context.filesDir}/videos/${videoId}/${folderContentNames.getOrNull(0)}"
+    val coroutineScope = CoroutineScope(Dispatchers.Default)
+
     Column(modifier = Modifier.fillMaxSize()) {
         Surface(color = colorPalette.ShortEaseRed
         ) {
@@ -88,40 +110,17 @@ fun PreviewScreen(
                 val context = LocalContext.current
                 val videoView = remember { VideoView(context) }
                 Column(modifier = Modifier.fillMaxSize()) {
-
-
                     Box(
                         modifier = Modifier
                             .weight(1f)
-                            .padding(8.dp)
+                            .padding(10.dp)
                             .clip(RoundedCornerShape(16.dp))
-                            .background(Color.Black),
+                            .background(Color.Black)
+                            .fillMaxSize(),
                         contentAlignment = Alignment.Center
                     ) {
-                        if(!isPlaying && videoId != null) {
-                            AndroidView(
-                                factory = { context ->
-                                    VideoView(context)
-                                },
-                                modifier = Modifier.fillMaxSize()
-                            ) { videoView ->
-                                val folder = File("${context.filesDir}/output/${videoId}/output-audio.mp4")
-                                val folderContents = folder.listFiles()
-                                val folderContentNames = folderContents?.map { file -> file.name } ?: emptyList()
-                                val video = File(folder, folderContentNames.getOrNull(0) ?: "")
-                                if(video.isFile) {
-                                    videoView.setVideoPath(video.absolutePath)
-                                    videoView.start()
-                                    val scope = CoroutineScope(Dispatchers.Default)
-                                    // Launch a coroutine
-                                    scope.launch {
-                                        // Wait for 30 seconds
-                                        delay(30000)
-                                        // Reset the variable
-                                        isPlaying = false
-                                    }
-                                }
-                            }
+                        if(folderContentNames.getOrNull(0) != null){
+                            VideoPlayer(finalVideoPath)
                         }
                     }
 
@@ -164,6 +163,7 @@ fun PreviewScreen(
                                 contentDescription = "Upload",
                             )
                         }
+
                     }
                 }
             }
@@ -175,4 +175,67 @@ fun PreviewScreen(
 @Preview
 private fun PreviewScreenPreview() {
     PreviewScreen(navController = rememberNavController())
+}
+
+@Composable
+fun VideoPlayer(videoPath : String) {
+    Log.d("video to see ", videoPath)
+    val context = LocalContext.current
+    val player = SimpleExoPlayer.Builder(context).build()
+    playerView = PlayerView(context)
+    val mediaItem = MediaItem.fromUri(videoPath)
+    val playWhenReady by rememberSaveable {
+        mutableStateOf(true)
+    }
+    player.setMediaItem(mediaItem)
+    playerView.player = player
+
+    val colorMatrix = ColorMatrix().apply {
+        // Example: Increase video brightness by 50%
+        setScale(10f, 10f, 10f, 1f)
+    }
+
+    LaunchedEffect(player) {
+        player.prepare()
+        Log.d("video to see ", videoPath)
+        Log.d("time", videoPath)
+        player.addListener(object : Player.EventListener {
+            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+                super.onPlayerStateChanged(playWhenReady, playbackState)
+                if (playbackState == Player.STATE_READY) {
+                    if(videoDuration == -1f) {
+                        endCropTime = player.duration.toFloat()
+                        videoDuration = endCropTime / 1000
+                    }
+                }
+            }
+        })
+        player.playWhenReady = playWhenReady
+        while (true) {
+            if (endCropTime > 0f && (player.currentPosition.toFloat() > endCropTime)) {
+                player.pause()
+                player.seekTo(startCropTime.toLong())
+            }
+            else if(player.currentPosition.toFloat() < startCropTime) {
+                player.seekTo(startCropTime.toLong())
+            }
+            delay(100) // Adjust the delay interval as needed
+        }
+    }
+    DisposableEffect(Unit) {
+        onDispose {
+            player.clearMediaItems()
+            player.release()
+        }
+    }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        AndroidView(
+            factory = {
+                playerView
+            }
+        )
+    }
 }
